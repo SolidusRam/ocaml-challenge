ed5ef4bd59ecf415df15e959f59589036fc9b9e3
SolidusRam <lemaildipara@gmail.com>
2024-10-07 09:51:18 +0200

    modificato nand per print di controllo

--- 1/nand/nand/bin/main.ml
+++ 1/nand/nand/bin/main.ml
@@ -1,5 +1,7 @@
 let () = print_endline "Hello, World! Ciao, Mondo!";;
 
+let a= true;;
+let b= false;;
 (* nand function utilizzando le espressionni logiche not e &&*)
 let nand a b = not (a && b);;
 
@@ -15,3 +17,7 @@ let nand3 a b =
   match (a, b) with
   | (true, true) -> false
   | _ -> true;;
+
+print_endline (string_of_bool (nand a b));;
+print_endline (string_of_bool (nand2 a b));;
+print_endline (string_of_bool (nand3 a b));;
