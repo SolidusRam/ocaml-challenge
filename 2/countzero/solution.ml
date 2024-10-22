72a200d297601ae0b3deddb0b2cc93a27ef9c612
SolidusRam <lemaildipara@gmail.com>
2024-10-22 22:21:01 +0200

    solved countzero

---
+++ 2/countzero/solution.ml
@@ -0,0 +1,15 @@
+let countzero f a b =
+  let rec countzero' f a b =
+    if a > b then 0
+    else if f a = 0 then 1 + countzero' f (a + 1) b
+    else countzero' f (a + 1) b
+  in countzero' f a b ;;
+
+(* Test cases *)
+assert (countzero (fun x -> x) (-10) 10 = 1);;
+
+assert (countzero (fun x -> x) 1 10 = 0);;
+
+assert (countzero (fun x -> x*x - 1) (-10) 10 = 2);;
+
+assert (countzero (fun x -> (if x<0 then -x else x) - 1) (-10) 10 = 2);;
